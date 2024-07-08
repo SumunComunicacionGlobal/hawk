acf9c66f91da5d5b39106a4194838bb8b2dbf460
Fernando Blasco Martín <37153521+FernandoSumun@users.noreply.github.com>
2024-07-09 00:03:16 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<div class="row align-items-center">
 
-			<div class="col-md-6">
+			<div class="col-md-6 col-lg-4">
 
 				<footer class="site-footer" id="colophon">
 
@@ -41,7 +41,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			</div><!--col end -->
 
-			<div class="col-md-6">
+			<div class="col-md-6 col-lg-8">
 
 				<nav id="legal-nav" class="navbar navbar-expand navbar-light" aria-labelledby="legal-nav-label">
 
